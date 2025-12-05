246e7d496be9bd24b4cc78d8c857b1cf0d337177
irajmoradi <irajjmoradi@gmail.com>
2025-12-05 14:57:46 -0800

    PUZZLE 2 DONE

--- solution.py
+++ solution.py
@@ -1,4 +1,38 @@
 import argparse
+import math
+
+
+def _find_zeros_and_pos(instruction: str, currentpos: int) -> (int, int):
+    """
+    Given an instruction and position it calculates the number of times we pass zero, and the new position
+
+    Args:
+        Instruction str: "R43" Dial goes "right" 43 positions
+        Currentpos int: a integer indicating where the dial is at now
+
+    Returns:
+        num_zeros and new position (int int)
+    """
+    magnitude = int(instruction[1:])
+    if instruction[0] == "R":
+        newpos = (magnitude + currentpos) % 100
+        print(magnitude+currentpos)
+        num_zero = (magnitude + currentpos) // 100
+
+    if instruction[0] == "L":
+        if currentpos == 0:
+            newpos = (100 - magnitude) % 100
+            num_zero = (magnitude // 100)
+        elif magnitude > currentpos:
+            newpos = (100 - (magnitude - currentpos)) % 100
+            num_zero = ((magnitude - currentpos) // 100) + 1 
+        else:
+            newpos = currentpos - magnitude
+            if newpos == 0:
+                num_zero = 1
+            else:
+                num_zero = 0
+    return num_zero, newpos
 
 
 def find_combo_two(instruct: list[str]) -> int:
@@ -7,44 +41,31 @@ def find_combo_two(instruct: list[str]) -> int:
     The dial starts at 50.
 
     Args:
-        instruct (list[str]): list of instructions, where the first character is the direction the lock/dial is turning, and the following 1 or 2 digit number is the number of times the dial is turned. 
+        instruct (list[str]): list of instructions, where the first character is the direction the lock/dial is turning, and the following 1 or 2 digit number is the number of times the dial is turned.
 
-    Returns: 
+    Returns:
         int: number of times the number 0 is passed on the dial during the following of the instructions
     """
     retval = 0
     currentpos = 50
-    print("TWO")
+    print("RAH")
     for ins in instruct:
-        print("Current position is ", currentpos)
-        print("instruction is ", ins)
-        print("Retval is ", retval)
-        if ins[0] == "L":
-            direction = -1
-        else:
-            direction = 1
-        # for L38 shift should be -38, and for R38 shift should be 38
-        shift = direction * int(ins[1:])
-        intpos = currentpos + shift
-        while intpos > 99 or intpos < 0:
-            if intpos < 0:
-                retval += 1
-                intpos = 100 + intpos
-            if intpos > 99:
-                retval += 1
-                intpos = intpos - 100
-        currentpos = intpos
-        print("---------------")
+        print(retval, ins, currentpos)
+        delta_retval, currentpos = _find_zeros_and_pos(ins, currentpos)
+        retval += delta_retval
+        print(retval, currentpos)
     return retval
+
+
 def find_combo(instruct: list[str]) -> int:
     """
     Calculates the number of zero's we pass on a 99 dial combination lock.
     The dial starts at 50.
 
     Args:
-        instruct (list[str]): list of instructions, where the first character is the direction the lock/dial is turning, and the following 1 or 2 digit number is the number of times the dial is turned. 
+        instruct (list[str]): list of instructions, where the first character is the direction the lock/dial is turning, and the following 1 or 2 digit number is the number of times the dial is turned.
 
-    Returns: 
+    Returns:
         int: number of times the number 0 is passed on the dial during the following of the instructions
     """
     retval = 0
@@ -82,10 +103,10 @@ def main():
     print(f"The number of zeroes your instructions make the dial pass is {num_zero}")
     print(function)
 
+
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser()
-    parser.add_argument('--instructions', required=True, nargs="*")
-    parser.add_argument('--fun')
+    parser.add_argument("--instructions", required=True, nargs="*")
+    parser.add_argument("--fun")
     args = parser.parse_args()
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,62 @@
-from solution import find_combo, find_combo_two
+from solution import find_combo, find_combo_two, _find_zeros_and_pos
+import math
+
 
 def test_puzzle_1():
-    assert find_combo(["L68", "L30", "R48", "L5", 
-                       "R60", "L55", "L1", "L99", 
-                       "R14", "L82"]) == 3
+    assert (
+        find_combo(["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"])
+        == 3
+    )
+
 
 def test_puzzle_2():
-    assert find_combo_two(["L68", "L30", "R48", "L5", 
-                       "R60", "L55", "L1", "L99", 
-                       "R14", "L82"]) == 6
+    assert (
+        find_combo_two(
+            ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
+        )
+        == 6
+    )
+
+
+#def test_puzzle_3():
+ #   number = int(
+  #      math.floor(423423 - 50) / 100
+  #      + int(math.floor((132412 + (423423 - 50) % 100) / 100))
+  #  )
+  #  assert find_combo_two(["L423423", "R132412"]) == number
+
+
+def test_puzzle_4():
+    # Hint used from https://www.reddit.com/r/adventofcode/comments/1pdq3yc/comment/ns6wkcv/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    assert find_combo_two(["L50", "R101"]) == 2
+
+
+def test_puzzle_5():
+    assert _find_zeros_and_pos("L55", 55) == (1, 0)
+
+
+def test_puzzle_6():
+    assert _find_zeros_and_pos("R48", 52) == (1, 0)
+
+
+def test_puzzle_7():
+    assert _find_zeros_and_pos("R48", 53) == (1, 1)
+
+
+def test_puzzle_8():
+    assert _find_zeros_and_pos("R148", 53) == (2, 1)
+
+
+def test_puzzle_9():
+    assert _find_zeros_and_pos("L155", 55) == (2, 0)
+
+
+def test_puzzle_10():
+    assert _find_zeros_and_pos("L55", 54) == (1, 99)
+
+
+def test_puzzle_11():
+    assert _find_zeros_and_pos("L155", 54) == (2, 99)
+
+def test_puzzle_12():
+    assert _find_zeros_and_pos("L5", 0) == (0, 95)
